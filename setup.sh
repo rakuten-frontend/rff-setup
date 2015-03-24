1ab6313cd78af0afcc10b774539518740563cea7
Tsutomu Ogasawara <tsutomu.ogasawara@mail.rakuten.com>
2015-03-24 15:02:02 +0900

    Initial.

---
+++ setup.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+# variables
+NODEJS_SITE="http://nodejs.org/"
+LOGFILE="setup.log"
+
+# Node.js
+if [ `which node` -a `which npm` ]; then
+    echo " node:" `node -v`
+    echo "  npm:" `npm -v`
+else
+    echo "Node.js is not installed. Please install it form the site."
+    open $NODEJS_SITE
+    exit 1
+fi
+
+echo "" > $LOGFILE
+
+# yeoman
+sudo npm install -g yo >> $LOGFILE 2>&1
+if [ $? -eq 0 ]; then
+    echo "   yo:" `yo --version`
+else
+    exit 1
+fi
+
+# Grunt
+sudo npm install -g grunt-cli >> $LOGFILE 2>&1
+if [ $? -eq 0 ]; then
+    echo "grunt:" `grunt --version`
+else
+    exit 1
+fi
+
+# Bower
+sudo npm install -g bower >> $LOGFILE 2>&1
+if [ $? -eq 0 ]; then
+    echo "bower:" `bower --version`
+else
+    exit 1
+fi
+
+# Sass
+sudo gem install sass >> $LOGFILE 2>&1
+if [ $? -eq 0 ]; then
+    echo "sass: " `sass --version`
+else
+    exit 1
+fi
+    
+# rff-doctor
+sudo npm install -g rff-doctor >> $LOGFILE 2>&1
+if [ $? -eq 0 ]; then
+    rff-doctor
+else
+    exit 1
+fi
+
+exit 0
